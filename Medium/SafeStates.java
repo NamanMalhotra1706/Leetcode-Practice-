70a9de3b69cf8052aa644cd9b9c9b72a285821e8
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2023-07-12 21:33:13 +0530

    [200~802. Find Eventual Safe States

---
+++ Medium/SafeStates.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafeStates {
+    public boolean depthSearch(List<List<Integer>> adj, int src, boolean[] vis, boolean[] recst) {
+        vis[src] = true;
+        recst[src] = true;
+        for (int x : adj.get(src)) {
+            if (!vis[x] && depthSearch(adj, x, vis, recst)) {
+                return true;
+            } else if (recst[x]) {
+                return true;
+            }
+        }
+        recst[src] = false;
+        return false;
+    }
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < graph[i].length; j++) {
+                list.add(graph[i][j]);
+            }
+            adj.add(list);
+        }
+        boolean[] vis = new boolean[n];
+        boolean[] recst = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                depthSearch(adj, i, vis, recst);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < recst.length; i++) {
+            if (!recst[i]) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
